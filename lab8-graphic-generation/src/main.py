4db572fcfbdde9f0845ebe89f6647c2ca8f16329
Richard <richard.staszkiewicz@gmail.com>
2022-06-09 19:14:10 +0200

    Lab8 files added

---
+++ lab8-graphic-generation/src/main.py
@@ -0,0 +1,7 @@
+import moderngl_window
+
+from base_window import BaseWindowConfig
+from robot_window import RobotWindow
+
+if __name__ == '__main__':
+    moderngl_window.run_window_config(BaseWindowConfig)
