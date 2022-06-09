046aefcd1f2dcfcdeb6708890f5bc3098f76279a
Richard <richard.staszkiewicz.stud@pw.edu.pl>
2022-06-09 20:08:07 +0200

    Pink floating head

--- lab8-graphic-generation/src/main.py
+++ lab8-graphic-generation/src/main.py
@@ -1,7 +1,6 @@
 import moderngl_window
 
-from base_window import BaseWindowConfig
 from robot_window import RobotWindow
 
 if __name__ == '__main__':
-    moderngl_window.run_window_config(BaseWindowConfig)
+    moderngl_window.run_window_config(RobotWindow)
--- lab8-graphic-generation/src/robot_window.py
+++ lab8-graphic-generation/src/robot_window.py
@@ -1,5 +1,5 @@
 import moderngl
-from pyrr import Matrix44
+from pyrr import Matrix44, Vector3
 
 from base_window import BaseWindowConfig
 
@@ -10,12 +10,19 @@ class RobotWindow(BaseWindowConfig):
         super(RobotWindow, self).__init__(**kwargs)
 
     def model_load(self):
-        pass
-        # TODO: Write model loading
+        # downloading objects
+        self.cube_obj = self.load_scene("cube.obj")
+        self.sphere_obj = self.load_scene("sphere.obj")
+
+        # added - shortcuts for rendering commands
+        self.cube_link = self.cube_obj.root_nodes[0].mesh.vao.instance(self.program)
+        self.sphere_link = self.sphere_obj.root_nodes[0].mesh.vao.instance(self.program)
+
 
     def init_shaders_variables(self):
-        pass
-        # TODO: Write init shader variables
+        self.var_color = self.program["color"]
+        self.var_pvm = self.program["pvm"]
+
 
     def render(self, time: float, frame_time: float):
         self.ctx.clear(0.8, 0.8, 0.8, 0.0)
@@ -27,4 +34,14 @@ class RobotWindow(BaseWindowConfig):
             (0.0, 0.0, 1.0),
             (0.0, 0.0, 1.0),
         )
-        # TODO: Write render part
+
+        # Head
+        # Set the color to pink
+        self.var_color.value = (0.9, 0.0, 0.9)
+
+        # Apply translation bu (0, 0, 5)
+        move = Matrix44.from_translation((0.0, 0.0, 5.0))
+
+        self.var_pvm.write((projection * lookat * move).astype("f4"))
+        self.sphere_link.render()
+
